a255b3e9297dd55331973add6d80641a1abd5033
francescdevy <erkkrey@gmail.com>
2026-01-28 21:09:55 +0100

    Update about.php

---
+++ hu/about.php
@@ -0,0 +1,20 @@
+<?php
+$page = 'about';
+
+$meta = [
+  'hu' => [
+    'about' => [
+      'title' => 'Rólunk – Cégnév',
+      'description' => 'Ismerje meg cégünket és szolgáltatásainkat.'
+    ]
+  ],
+  'en' => [
+    'about' => [
+      'title' => 'About Us – Company Name',
+      'description' => 'Learn more about our company and services.'
+    ]
+  ]
+];
+
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
+?>
