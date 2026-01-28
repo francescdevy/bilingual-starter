5162ac203419efcd6934f139031fdf5aaaf6f31b
francescdevy <erkkrey@gmail.com>
2026-01-28 21:13:14 +0100

    Update index.php

---
+++ hu/index.php
@@ -0,0 +1,10 @@
+<?php
+$page = 'home';
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/includes/nav.php';
+?>
+
+<h1><?= $L['home.h1'] ?></h1>
+<p><?= $L['home.desc'] ?></p>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
