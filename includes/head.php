f53d0a043ab4af9df16f75d9a497b5ea7060dd7c
francescdevy <erkkrey@gmail.com>
2026-01-28 21:10:20 +0100

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,1 +1,12 @@
-{\rtf1}
+<!DOCTYPE html>
+<html lang="<?= $lang ?>">
+<head>
+  <meta charset="utf-8">
+  <title><?= $meta[$lang][$page]['title'] ?></title>
+  <meta name="description" content="<?= $meta[$lang][$page]['description'] ?>">
+
+  <link rel="alternate" hreflang="hu"
+        href="https://ceg.hu/hu/<?= $page ?>.php">
+  <link rel="alternate" hreflang="en"
+        href="https://ceg.hu/en/<?= $page ?>.php">
+</head>
