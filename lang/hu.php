c861343170626fc565fcf2b6d866957e4a15d2fc
francescdevy <erkkrey@gmail.com>
2026-01-28 21:11:37 +0100

    Update hu.php

--- lang/hu.php
+++ lang/hu.php
@@ -1,4 +1,14 @@
+<?php
 return [
+  'site.name' => 'Cégnév',
+  'nav.home'  => 'Főoldal',
+  'nav.about' => 'Rólunk',
+
+  'home.title' => 'Főoldal – Cégnév',
+  'home.desc'  => 'Megbízható megoldások cégeknek.',
+  'home.h1'    => 'Üdvözöljük!',
+
   'about.title' => 'Rólunk – Cégnév',
-  'about.desc'  => 'Ismerje meg cégünket...'
+  'about.desc'  => 'Ismerje meg cégünket.',
+  'about.h1'    => 'Rólunk'
 ];
