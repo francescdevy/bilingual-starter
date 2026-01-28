d9f71ee962198f8173343477e7081d45b8c1058c
francescdevy <erkkrey@gmail.com>
2026-01-28 21:12:29 +0100

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -1,12 +1,26 @@
+<?php
+$lang = explode('/', trim($_SERVER['REQUEST_URI'], '/'))[0] ?? 'hu';
+$page = $page ?? 'home';
+
+$L = require __DIR__ . "/../lang/$lang.php";
+
+$title = $L["$page.title"] ?? $L['site.name'];
+$desc  = $L["$page.desc"]  ?? '';
+?>
 <!DOCTYPE html>
 <html lang="<?= $lang ?>">
 <head>
   <meta charset="utf-8">
-  <title><?= $meta[$lang][$page]['title'] ?></title>
-  <meta name="description" content="<?= $meta[$lang][$page]['description'] ?>">
+  <title><?= $title ?></title>
+  <meta name="description" content="<?= $desc ?>">
 
-  <link rel="alternate" hreflang="hu"
-        href="https://ceg.hu/hu/<?= $page ?>.php">
-  <link rel="alternate" hreflang="en"
-        href="https://ceg.hu/en/<?= $page ?>.php">
+  <link rel="alternate" hreflang="hu" href="/hu/<?= $page ?>.php">
+  <link rel="alternate" hreflang="en" href="/en/<?= $page ?>.php">
+
+  <style>
+    body { font-family: sans-serif; padding: 40px; }
+    nav a { margin-right: 10px; text-decoration: none; }
+    .lang a.active { font-weight: bold; text-decoration: underline; }
+  </style>
 </head>
+<body>
