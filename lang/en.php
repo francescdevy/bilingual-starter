de28cd259b9fedd93a9ac4d4fe3e0abed532f1d5
francescdevy <erkkrey@gmail.com>
2026-01-28 21:11:56 +0100

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -1,4 +1,14 @@
+<?php
 return [
+  'site.name' => 'Company Name',
+  'nav.home'  => 'Home',
+  'nav.about' => 'About Us',
+
+  'home.title' => 'Home – Company Name',
+  'home.desc'  => 'Reliable solutions for businesses.',
+  'home.h1'    => 'Welcome!',
+
   'about.title' => 'About Us – Company Name',
-  'about.desc'  => 'Learn more about our company...'
+  'about.desc'  => 'Learn more about our company.',
+  'about.h1'    => 'About Us'
 ];
